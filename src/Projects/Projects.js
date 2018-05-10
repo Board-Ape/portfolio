b4e0e8878a5f9d222fcbe01501c6d237cd7dee19
Sam Singer <samuel.singer1@gmail.com>
2018-05-10 15:42:14 -0700

    Enigma addition to projects

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -35,7 +35,18 @@ const Projects = () => {
             <a className="live-link" href="http://movie-tracker-sys.surge.sh/" target="_blank" rel="noopener noreferrer">Live Site</a>
             <a href="https://github.com/Cache123/movie-tracker-2.0" target="_blank" rel="noopener noreferrer">Github Code</a>
             <hr className="software-dash spacing" />
-          <h2 className="project-3 projects-title">Accordian Layout</h2>
+          <h2 className="project-3 projects-title">Enigma</h2>
+            <img className="desktop" src={ require('./project-screen-shots/enigma-opening.png') } alt="Desktop screen size." />
+            <img className="desktop" src={ require('./project-screen-shots/enigma.png') } alt="Desktop screen size." />
+            <br />
+            <p className="project-description">
+              <strong>Enigma</strong> is a game rendition of the mobile hit Flappy Bird. Capabilities include: custom artwork, collision detection/real
+              world physics, score tracking. Utilizing JavaSript, jQuery, HTML, and CSS.
+            </p>
+            <a className="live-link" href="https://enigma-sys.surge.sh/" target="_blank" rel="noopener noreferrer">Live Site</a>
+            <a href="https://github.com/JorgeEdPerezGa/game-time" target="_blank" rel="noopener noreferrer">Github Code</a>
+            <hr className="software-dash spacing" />
+          <h2 className="project-4 projects-title">Accordian Layout</h2>
             <img className="desktop" src={ require('./project-screen-shots/accordian.png') } alt="Desktop screen size." />
             <img className="mobile" src={ require('./project-screen-shots/accordian-mobile.png') } alt="Mobile screen size." />
             <br />
@@ -46,7 +57,7 @@ const Projects = () => {
             <a className="live-link" href="https://cache123.github.io/pattern-party/" target="_blank" rel="noopener noreferrer">Live Site</a>
             <a href="https://github.com/Cache123/pattern-party" target="_blank" rel="noopener noreferrer">Github Code</a>
             <hr className="software-dash spacing" />
-          <h2 className="project-4 projects-title">Swapi Box</h2>
+          <h2 className="project-5 projects-title">Swapi Box</h2>
             <img className="desktop" src={ require('./project-screen-shots/swapi-box.png') } alt="Desktop screen size." />
             <img className="mobile" src={ require('./project-screen-shots/swapi-box-mobile.png') } alt="Mobile screen size." />
             <br />
